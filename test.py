1311c0dc2cc4ccc824bc5bc59d384e514ec3c97e
FrederikMS <Frederik.moller2@hotmail.com>
2025-04-26 17:00:35 +0200

    stuck on transfer function...

--- test.py
+++ test.py
@@ -1,27 +1,110 @@
 import os
 import json
+import ndjson
 import requests
 import google.oauth2.id_token
 import google.auth.transport.requests
+from google.cloud import storage
+import pandas as pd
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../pynes-case-50f5db6cfad9.json'
-request = google.auth.transport.requests.Request()
-audience = 'https://rawdataget-978661416923.us-central1.run.app/rawDogBreedDataGet'
-TOKEN = google.oauth2.id_token.fetch_id_token(request, audience)
+def main():
+    storeDataToBucket()
 
-r = requests.post(
-    audience, 
-    headers={'Authorization': f"Bearer {TOKEN}", "Content-Type": "application/json"},
-    json=json.dumps({"key": "value"})  # possible request parameters
-)
+def getRawData():
+    '''
+        Calls google cloud and gets the raw data.
+    '''
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../pynes-case-50f5db6cfad9.json'
+    request = google.auth.transport.requests.Request()
+    audience = 'https://rawdataget-978661416923.us-central1.run.app/rawDogBreedDataGet'
+    TOKEN = google.oauth2.id_token.fetch_id_token(request, audience)
 
-# Convert json into dictionary 
-response_dict = r.json()
-  
-# Pretty Printing JSON string back 
-print(json.dumps(response_dict, indent=4, sort_keys=True))
+    r = requests.post(
+        audience, 
+        headers={'Authorization': f"Bearer {TOKEN}", "Content-Type": "application/json"},
+        json=json.dumps({"key": "value"})  # possible request parameters
+    )
+
+    # Convert json into dictionary 
+    return r.text
+    # response_dict = r.json()
+    
+    # # Pretty Printing JSON string back 
+    # return json.dumps(response_dict, indent=4, sort_keys=True)
+
+def storeDataToBucket():
+    """Write and read a blob from GCS using file-like IO"""
+    # The ID of your GCS bucket
+    bucket_name = "dog-breed-data"
+
+    # The ID of your new GCS object
+    blob_name = "dog-breed-data-raw-new-test"
+
+    # The data to store in the bucket
+    data = getRawData()
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    with blob.open("w") as f:
+        f.write(data)
+
+def read_from_bucket():
+    """Write and read a blob from GCS using file-like IO"""
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../pynes-case-50f5db6cfad9.json'
+    request = google.auth.transport.requests.Request()
+    audience = 'https://rawdataget-978661416923.us-central1.run.app/read_from_bucket'
+    TOKEN = google.oauth2.id_token.fetch_id_token(request, audience)
+
+    r = requests.post(
+        audience, 
+        headers={'Authorization': f"Bearer {TOKEN}", "Content-Type": "application/json"}
+        )
+
+    # Convert json into dictionary 
+    response_dict = r.json()
+    
+    # Pretty Printing JSON string back 
+    return json.dumps(response_dict, indent=4, sort_keys=True)
+
+def delete_blob(blob_name):
+    """Deletes a blob from the bucket."""
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../pynes-case-50f5db6cfad9.json'
+    request = google.auth.transport.requests.Request()
+    audience = 'https://rawdataget-978661416923.us-central1.run.app/delete_blob'
+    TOKEN = google.oauth2.id_token.fetch_id_token(request, audience)
+
+    r = requests.delete(
+        audience, 
+        headers={'Authorization': f"Bearer {TOKEN}", "Content-Type": "application/json"}
+        )
+
+    # return status and reason
+    return r.status_code, r.reason
+
+def write_to_table():
+    """Deletes a blob from the bucket."""
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../pynes-case-50f5db6cfad9.json'
+    request = google.auth.transport.requests.Request()
+    audience = 'https://rawdataget-978661416923.us-central1.run.app/write_to_table'
+    TOKEN = google.oauth2.id_token.fetch_id_token(request, audience)
+
+    r = requests.post(
+        audience, 
+        headers={'Authorization': f"Bearer {TOKEN}", "Content-Type": "application/json"}
+        )
+
+    # return status and reason
+    return r.status_code, r.reason
 
 
-# print(requests.get("https://api.thedogapi.com/v1/breeds").text)
-# r1 = requests.get("https://api.thedogapi.com/v1/breeds")
-# print(r1.status_code, r1)
+
+if __name__ == "__main__":
+    #storeDataToBucket()
+    r = requests.get("https://api.thedogapi.com/v1/breeds")
+    #text = ndjson.dumps(r.json())
+    #formatted_data = ndjson.loads(text)
+    pd.read_json(r)
+    result = [json.dumps(record) for record in r.json()]
+    print(result)
