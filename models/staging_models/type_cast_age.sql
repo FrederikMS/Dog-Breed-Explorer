6bf267d6ecb75f3f0ecfbc5a30dff60c29dbe085
Frederik Sørensen <Frederik.moller2@hotmail.com>
2025-04-28 20:29:28 +0000

    made staging models

---
+++ models/staging_models/type_cast_age.sql
@@ -0,0 +1,17 @@
+with source_data as (select * from {{ ref("type_cast_height_and_weight") }})
+
+select
+    *,
+    case
+        when instr(life_span, '-') - 1 > 0
+        then cast(substr(life_span, 1, instr(life_span, '-') - 1) as integer)
+        when instr(life_span, '–') - 1 > 0
+        then cast(substr(life_span, 1, instr(life_span, '–') - 1) as integer)
+        when instr(life_span, '–') - 1 < 0 and instr(life_span, '-') - 1 < 0
+        then cast(regexp_substr(life_span, '^[0-9]*') as integer)
+        else null
+    end as life_span_lower,
+    /* using regex to extract last numer*/
+    array_reverse(regexp_extract_all(life_span, r'\d+'))[offset(0)] as life_span_upper
+from source_data
+
